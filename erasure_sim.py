f13fe6266b6f075c684221372cdc88c5e03a8d08
Andrew Campbell <andrewcampbell@college.harvard.edu>
2014-11-10 14:04:59 -0500

    Added main project file

---
+++ erasure_sim.py
@@ -0,0 +1,26 @@
+
+"""
+CS109 Final Project
+Simulation of Erasure Code Performance in Low Reliability Networks 
+
+Jeff Rogers
+Nicolai Astrup Wiik
+Andrew Campbell
+Dawit Gebregziabher
+"""
+
+
+#Function: encrypt data of length k using Reed-Soloman code
+
+
+#Function: encypted block is divided into n pieces, k of which are needed for recovery
+
+
+#Distribute payload amoung n nodes
+
+
+#Link/node failure probability constants
+
+
+#Monte Carlo Simulation of Reed-Soloman
+
